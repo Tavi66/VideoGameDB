eca56b7c68e1c26229fdca84e3ed4f0fbf4b8400
Tavi66 <42556132+Tavi66@users.noreply.github.com>
2019-02-14 22:13:27 -0600

    Added videogame.sql

--- tables.sql
+++ tables.sql
@@ -3,13 +3,32 @@ CREATE SCHEMA VideoGameDB;
 DROP TABLE user;
 #create 'user' table
 CREATE TABLE user (
-username VARCHAR(20),
 name VARCHAR(20) NOT NULL,
+username VARCHAR(20),
 password VARCHAR(30) NOT NULL,
 privilege INT default 0,
 PRIMARY KEY (username)
 );
 
+CREATE TABLE videogame(
+Title  VARCHAR(50),
+ReleaseDate DATE,
+#Genre VARCHAR(30), #join w/ 'genre' table
+Price DOUBLE(5,2),
+#Platform VARCHAR(25), #join w/ 'platforms' table
+Rating VARCHAR(5),
+#Region VARCHAR(4),
+Series VARCHAR(50),
+LinkToCoverImage VARCHAR(256), 
+#Retailer VARCHAR(20); #join with 'retailer' table
+PRIMARY KEY (Title)
+);
+
+CREATE TABLE genre(
+videoGameTitle VARCHAR(50),
+Genre VARCHAR(30),
+CONSTRAINT FOREIGN KEY (videoGameTitle) REFERENCES videogame(Title)
+);
 #username (PK)
 #password not null
 #default value for privilege = 0 (standard user)
--- user.sql
+++ user.sql
@@ -2,19 +2,28 @@
 SELECT * FROM user;
 
 #DELETE a user with a username
-DELETE FROM user WHERE username='lily';
+DELETE FROM user WHERE username='reyner';
 
 #INSERT into user table
-#All 4 entries inserted.
 #Administrator user. Admin privilege entered.
 INSERT INTO user (name,username,password,privilege)
 VALUES ('vi','vi','queen',1);
 INSERT INTO user (name,username,password,privilege)
 VALUES ('dzung','dzung','chronos',1);
-#3 entries entered. Standard user. default privilege used.
+#Standard user. default privilege used.
 INSERT INTO user (name,username,password)
 VALUES ('N','lily','Alpha');
+INSERT INTO user (name,username,password, privilege)
+VALUES ('AAA','reyner','Beta', 0);
 
 #UPDATE user table
+#update name where username
+UPDATE user
+SET  name = 'wan'
+WHERE username = 'vi';
+#update password where username
+UPDATE user
+SET  password = 'noir'
+WHERE username = 'dzung';
 
 #JOIN
